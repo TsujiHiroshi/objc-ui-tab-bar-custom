cfd11874297eac76c52ea015be3273b72b1254c2
Masaki Hirokawa <info@dolice.net>
2013-09-05 18:53:02 +0900

    My First Commit

---
+++ UITabBarCustomSample/UITabBarCustomSample/Classes/Screen/FifthViewController.m
@@ -0,0 +1,32 @@
+//
+//  FifthViewController.m
+//  UITabBarCustomSample
+//
+//  Created by 廣川政樹 on 2013/09/05.
+//  Copyright (c) 2013年 Masaki Hirokawa. All rights reserved.
+//
+
+#import "FifthViewController.h"
+
+@interface FifthViewController ()
+
+@end
+
+@implementation FifthViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    //背景指定
+    self.view.backgroundColor = [UIColor underPageBackgroundColor];
+    
+    //タイトルラベル配置
+    UILabel *titleLabel = [DCLabel planeLabel:CGRectMake(0, 0, 320, 48)
+                                         text:@"FifthViewController" font:[UIFont fontWithName:@"Helvetica" size:20]
+                                    textColor:[UIColor whiteColor] textAlignment:NSTextAlignmentCenter
+                              backgroundColor:[UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.5f]];
+    [self.view addSubview:titleLabel];
+}
+
+@end
